7fb6b42a61f9d602a10dcbf3527ae29f4ed5d2fb
Dev Aryan Dogra <devdogra1@gmail.com>
2024-01-27 13:58:35 +0530

    Integrate clerk authentication

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,6 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
+      <UserButton afterSignOutUrl="/" />
       <h1 className="h1-bold">Hello World</h1>
     </div>
   );
